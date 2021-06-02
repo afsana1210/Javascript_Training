9e470011228ff96cda7c1167600c12745a1fd6ad
afsana1210 <afsanaansari1210@gmail.com>
2021-06-02 10:46:29 +0530

    Document object model

---
+++ DOM/main.js
@@ -0,0 +1,16 @@
+//console.dir(document);
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+// //document.title=123;
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);
+// console.log(document.all[10]);
+// console.log(document.forms[0]);
+// console.log(document.links);
+const ul=document.querySelector('.item');
+//ul.remove();
+//ul.lastElementChild.remove();
+ul.firstElementChild.textContent="hello";
